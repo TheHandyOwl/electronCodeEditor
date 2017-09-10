2c65e0a078b289002408c4d83d5e2e7e1b0b4835
Carlos <thehandyowl.io@gmail.com>
2017-09-10 03:20:35 +0200

    Loading file. Reimplementing drag & drop on will-navigate

--- index.js
+++ index.js
@@ -6,4 +6,16 @@ app.on('ready', () => {
     let mainWindow = new BrowserWindow();
 
     mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+    mainWindow.webContents.on('will-navigate', (e, url) => {
+        e.preventDefault(); // To cancel de default event
+        //console.log(url);
+
+        mainWindow.webContents.send('open-file', url);
+    });
+
+    mainWindow.webContents.on('devtools-opened', (e) => {
+        console.log('devtools opened. Use at your own risk');
+    });
+
 });
--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-console.log("Hello world from main.js");
+// console.log("Hello world from main.js");
 
 const loader = require('monaco-loader');
+const { ipcRenderer } = require('electron');
+const fs = require('fs');
 
 loader().then( monaco => {
+
     let editor = monaco.editor.create(
         document.querySelector('#app'),
         {
@@ -10,5 +13,18 @@ loader().then( monaco => {
             theme: 'vs-dark',
             automaticLayout: true
         }
-    )
+    );
+
+    ipcRenderer.on('open-file', (e, url) => {
+        //console.log(url);
+        fs.readFile(url.slice(7), 'utf8', (err, data) => {
+            if (err) {
+                console.log(err);
+                return;
+            } else {
+                editor.setModel(monaco.editor.createModel(data, 'javascript'));
+            }
+        });
+    });
+
 });
